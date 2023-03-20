c27a8ee80627bb062149c825626fc18243705d8b
jlangwerska <jlangwerska@gmail.com>
2023-03-20 19:44:43 +0100

    change to bolean

--- greater.py
+++ greater.py
@@ -1,8 +1,10 @@
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-if x > y:
-    print("x is greater than y")
-elif x < y:
-    print("y is greater than x")
-else:
-    print("both are equal")
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+print(y > x)
+
+# if x > y:
+#     print("x is greater than y")
+# elif x < y:
+#     print("y is greater than x")
+# else:
+#     print("both are equal")
